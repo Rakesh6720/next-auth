4b30e6171b302628ff32d692b29207d8ec496fa4
Rakesh Baruah <rbaruah@crisisprevention.com>
2023-12-12 10:06:20 -0600

    complete nav bar with shadcn

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,9 +1,15 @@
+import { buttonVariants } from '@/components/ui/button'
+import { HandMetal } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 function Navbar() {
     return (
         <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed top-0 w-full z-10'>
-            <div className='container'>Login</div>
+            <div className='container flex items-center justify-between'>
+                <Link href="/"><HandMetal /></Link>
+                <Link href="/sign-in" className={buttonVariants()}>Sign in</Link>
+            </div>
         </div>
     )
 }
