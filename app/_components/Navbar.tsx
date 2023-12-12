70d6580e94bf2e82435d4412483751f7c3603185
Rakesh Baruah <rbaruah@crisisprevention.com>
2023-12-12 09:59:54 -0600

    set base project styles

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='bg-zinc-100 py-2 border-b border-s-zinc-200 fixed top-0 w-full z-10'>
+            <div className='container'>Login</div>
+        </div>
+    )
+}
+
+export default Navbar
