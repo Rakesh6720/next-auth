b702027b859e56551b2eaec2ade0f39d8955ebf9
Rakesh Baruah <rbaruah@gmail.com>
2023-12-14 03:44:06 -0600

    remove async from client component

--- app/_components/EventDetailsComponent.tsx
+++ app/_components/EventDetailsComponent.tsx
@@ -1,22 +1,25 @@
-"use client"
+"use client";
 
-import { Button } from '@/components/ui/button'
-import { addUserToEvent } from '@/lib/actions';
-import { Event } from '@prisma/client'
-import { Session } from 'next-auth';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { addUserToEvent } from "@/lib/actions";
+import { Event } from "@prisma/client";
+import React from "react";
 
-async function EventDetailsComponent({ event }: { event: Event }) {
-    const eventId = event?.id;
+function EventDetailsComponent({ event }: { event: Event }) {
+  const eventId = event?.id;
 
-    return (
-        <div>
-            <h1>{event?.name}</h1>
-            <Button onClick={() => {
-                addUserToEvent(Number(eventId))
-            }}>Attend</Button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>{event?.name}</h1>
+      <Button
+        onClick={() => {
+          addUserToEvent(Number(eventId));
+        }}
+      >
+        Attend
+      </Button>
+    </div>
+  );
 }
 
-export default EventDetailsComponent
+export default EventDetailsComponent;
